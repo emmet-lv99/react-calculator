0f5c60443f0a48301eb8432a6ffde3ca6c986fbb
emmet-lv99 <dachanisfull@gmail.com>
2021-12-22 20:09:19 +0900

    tmp

--- src/container/InterfaceContainer.js
+++ src/container/InterfaceContainer.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import calStore from '../CalStore'
 import Interface from '../components/Interface'
 
 const InterfaceContainer = () => {
-  const display = calStore.getState().display
+  const [display, setDisplay] = useState(calStore.getState().display)
+  calStore.subscribe(() => setDisplay(calStore.getState().display))
   const emitDisplay = val => {
     calStore.dispatch({ type: 'SET_DISPLAY', value: val })
   }
